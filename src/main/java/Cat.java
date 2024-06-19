02d45c73ce96f6b5d564a9cb688d16d7f1b07ca4
Valuev Semyon <valuevs466@gmail.com>
2024-06-20 00:17:55 +0300

    final version2

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -2,15 +2,6 @@ public class Cat {
     private String name;
 
 
-    public Cat() {
-
-    }
-
-    public Cat(String name) {
-        this.name = name;
-
-    }
-
     public String getName() {
         return name;
     }
